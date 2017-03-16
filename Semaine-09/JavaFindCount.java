0b0b65fa90bb6f7ffcf904c9e9aa42fe9d96d295
stm1886 <stm1886@hotmail.com>
2017-03-16 13:17:30 -0400

    Update JavaFindCount.java

--- Semaine-09/JavaFindCount.java
+++ Semaine-09/JavaFindCount.java
@@ -6,7 +6,7 @@ public class JavaFindCount {
   public static void main(String[] args) {
     try {
       MongoClient mongoClient = new MongoClient("localhost", 27017);
-      DB db = mongoClient.getDB("words");
+      DB db = mongoClient.getDB("semaine09");
       DBCollection collection = db.getCollection("word_stats");
       JavaFindCount.countWords(collection);
     } catch (Exception e){

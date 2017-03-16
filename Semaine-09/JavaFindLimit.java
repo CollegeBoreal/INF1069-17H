6042ed9e0171825f5ea78ffe718ca702f797b8aa
stm1886 <stm1886@hotmail.com>
2017-03-16 13:19:39 -0400

    Update JavaFindLimit.java

--- Semaine-09/JavaFindLimit.java
+++ Semaine-09/JavaFindLimit.java
@@ -8,7 +8,7 @@ public class JavaFindLimit {
   public static void main(String[] args) {
     try {
       MongoClient mongoClient = new MongoClient("localhost", 27017);
-      DB db = mongoClient.getDB("words");
+      DB db = mongoClient.getDB("semaine09");
       DBCollection collection = db.getCollection("word_stats");
       JavaFindLimit.limitResults(collection, 1);
       JavaFindLimit.limitResults(collection, 3);

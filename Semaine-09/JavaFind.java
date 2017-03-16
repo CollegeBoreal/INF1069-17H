c12053c681b3a4d11f7945d229a9fb1787b07031
stm1886 <stm1886@hotmail.com>
2017-03-16 13:17:11 -0400

    Update JavaFind.java

--- Semaine-09/JavaFind.java
+++ Semaine-09/JavaFind.java
@@ -8,7 +8,7 @@ public class JavaFind {
   public static void main(String[] args) {
     try {
       MongoClient mongoClient = new MongoClient("localhost", 27017);
-      DB db = mongoClient.getDB("words");
+      DB db = mongoClient.getDB("semaine09");
       DBCollection collection = db.getCollection("word_stats");
       JavaFind.getOne(collection);
       JavaFind.getManyWhile(collection);

394970faa3bf80dd3f7cc89fdb175f4e929607fa
stm1886 <stm1886@hotmail.com>
2017-03-16 13:24:32 -0400

    Update JavaGroup.java

--- Semaine-09/JavaGroup.java
+++ Semaine-09/JavaGroup.java
@@ -8,7 +8,7 @@ public class JavaGroup {
   public static void main(String[] args) {
     try {
       MongoClient mongoClient = new MongoClient("localhost", 27017);
-      DB db = mongoClient.getDB("words");
+      DB db = mongoClient.getDB("semaine09");
       DBCollection collection = db.getCollection("word_stats");
       JavaGroup.firstIsALastIsVowel(collection);
       JavaGroup.firstLetterTotals(collection);

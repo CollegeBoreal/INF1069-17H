ce51b39947cdc16137d5de1658db9bdb8bdc6f3b
stm1886 <stm1886@hotmail.com>
2017-03-16 23:40:29 -0400

    update

--- Semaine-09/JavaConnect.java
+++ Semaine-09/JavaConnect.java
@@ -1,18 +1,33 @@
+package semaine09;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.WriteConcern;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+
+/**
+ * INF1069-17H
+ * This class connects to MongoDB and prints the number of documents
+ * from collection called word_stats.
+ * By Steve Tshibangu <a>Steve.TshibanguMutshi@collegeboreal.ca</a>
+ */
 public class JavaConnect {
-  public static void main(String[] args) {
-    try {
-      MongoClient mongoClient = new MongoClient("localhost", 27017);
-      mongoClient.setWriteConcern(WriteConcern.JOURNAL_SAFE);
-      DB db = mongoClient.getDB("semaine09");
-      DBCollection collection = db.getCollection("word_stats");
-      System.out.println("Number of Documents: " + 
-        new Long(collection.count()).toString());
-    } catch (Exception e) {
-      System.out.println(e);
+    public static void main(String[] args) {
+        try {
+            MongoClient mongoClient = null;
+            MongoDatabase mongoDatabase = null;
+            MongoCollection collection = null;
+
+            // ***Change the server and port to match your database connection
+            mongoClient = new MongoClient("10.0.2.2", 27018);
+            mongoClient.setWriteConcern(WriteConcern.JOURNAL_SAFE);
+            mongoDatabase = mongoClient.getDatabase("semaine09");
+            collection = mongoDatabase.getCollection("word_stats");
+            System.out.println("Number of Documents: " +
+                    new Long(collection.count()).toString());
+        } catch (Exception e) {
+            // Print errors
+            System.out.println(e);
+        }
     }
-  }
 }
--- Semaine-09/JavaFind.java
+++ Semaine-09/JavaFind.java
@@ -1,52 +1,89 @@
-import com.mongodb.MongoClient;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
+package semaine09;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+/**
+ * INF1069-17H
+ * This class implements example for find function.
+ * By Steve Tshibangu <a>Steve.TshibanguMutshi@collegeboreal.ca</a>
+ */
 public class JavaFind {
-  public static void main(String[] args) {
-    try {
-      MongoClient mongoClient = new MongoClient("localhost", 27017);
-      DB db = mongoClient.getDB("semaine09");
-      DBCollection collection = db.getCollection("word_stats");
-      JavaFind.getOne(collection);
-      JavaFind.getManyWhile(collection);
-      JavaFind.getManyFor(collection);
-      JavaFind.getManyToArray(collection);
-    } catch (Exception e) {
-      System.out.println(e);
+    public static void main(String[] args) {
+        try {
+            MongoClient mongoClient = null;
+            MongoDatabase mongoDatabase = null;
+            MongoCollection collection = null;
+
+            // ***Change the server and port to match your database connection
+            mongoClient = new MongoClient("10.0.2.2", 27018);
+            mongoDatabase = mongoClient.getDatabase("semaine09");
+            collection = mongoDatabase.getCollection("word_stats");
+            JavaFind.getOne(collection);
+            JavaFind.getManyWhile(collection);
+            JavaFind.getManyFor(collection);
+            JavaFind.getManyToArray(collection);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-  }
-  public static void getOne(DBCollection collection){
-    DBObject doc = collection.findOne();
-    System.out.println("Single Document: ");
-    System.out.println(doc.toString());
-  }
-  public static void getManyWhile(DBCollection collection){
-    DBCursor cursor = collection.find();
-    Double count = 0.0;
-    while(cursor.hasNext()) {
-      DBObject doc = cursor.next();
-      count += Double.parseDouble(doc.get("size").toString());
+
+    public static void getOne(MongoCollection collection) {
+        Document document = (Document) collection.find().first();
+        System.out.println("Single Document: ");
+        System.out.println(document.toString());
     }
-    System.out.println("\nTotal characters using While loop: ");
-    System.out.println(count);
-  }
-  public static void getManyFor(DBCollection collection){
-    System.out.println("\nFor loop iteration: ");
-    DBCursor cursor = collection.find();    
-    for(Integer i=0; i<5; i++){
-      DBObject doc = cursor.next();
-      System.out.println(doc.get("word"));
+
+    public static void getManyWhile(MongoCollection collection) {
+        FindIterable<Document> iterable = null;
+        Iterator<Document> iterator = null;
+        Document document = null;
+        Double count = 0.0;
+
+        iterable = collection.find();
+        iterator = iterable.iterator();
+        while(iterator.hasNext()) {
+            document = iterator.next();
+            count += Double.parseDouble(document.get("size").toString());
+        }
+        System.out.println("\nTotal characters using While loop: ");
+        System.out.println(count);
     }
-  }
-  public static void getManyToArray(DBCollection collection){
-    System.out.println("\nConverted to array iteration: ");
-    DBCursor cursor = collection.find();
-    List<DBObject> docs = cursor.toArray(5);
-    for(final DBObject doc : docs) {
-      System.out.println(doc.get("word"));
+
+    public static void getManyFor(MongoCollection collection) {
+        FindIterable<Document> iterable = null;
+        Iterator<Document> iterator = null;
+        Document document = null;
+
+        System.out.println("\nFor loop iteration: ");
+        iterable = collection.find();
+        iterator = iterable.iterator();
+        for(Integer i=0; i<5; i++){
+            document = iterator.next();
+            System.out.println(document.get("word"));
+        }
     }
-  }
+
+    public static void getManyToArray(MongoCollection collection) {
+        FindIterable<Document> iterable = null;
+        Iterator<Document> iterator = null;
+        List<Document> documents = null;
+
+        System.out.println("\nConverted to array iteration: ");
+        iterable = collection.find();
+        iterator = iterable.iterator();
+        documents = new ArrayList<>();
+        iterator.forEachRemaining(documents::add);
+        for(final Document document : documents) {
+            System.out.println(document.get("word"));
+        }
+    }
 }
